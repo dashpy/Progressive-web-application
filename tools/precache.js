0fc37e638d0f62fb3711b88768c7dff12f4c879d
fede <fdra2323@gmail.com>
2017-07-29 02:22:09 -0400

    Updates

---
+++ tools/precache.js
@@ -0,0 +1,23 @@
+
+const name = 'PWADemo-v2'
+module.exports = {
+  staticFileGlobs: [
+    './../index.html',
+    './../images/*.{png,svg,gif,jpg}',
+    './../fonts/**/*.{woff,woff2}',
+    './../js/*.js',
+    './../css/*.css',
+    'https://fonts.googleapis.com/icon?family=Material+Icons'
+  ],
+  stripPrefix: '.',
+
+  runtimeCaching: [{
+    urlPattern: /https:\/\/api\.github\.com\/search\/repositories/,
+    handler: 'networkFirst',
+    options: {
+      cache: {
+        name: name
+      }
+    }
+  }]
+};
